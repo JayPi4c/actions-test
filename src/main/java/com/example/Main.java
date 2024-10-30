837dfd4ce1009d8fc777d9c57785bee8f645b93c
Jonas Pohl <jonas.jp4c@gmail.com>
2024-10-30 18:09:50 +0100

    Adding new line of code

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -3,5 +3,6 @@ package com.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("This does work much faster because it's cached!");
     }
 }
